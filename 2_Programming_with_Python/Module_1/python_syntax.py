a62a5ad4bf388a7cefac2d0cbbe60ae63d534691
Brian <wamwangimathenge@gmail.com>
2023-10-24 09:16:06 +0300

    Correctly indent and space code
    This commit demonstrates the significance of indentation and spaces in
    python code.

---
+++ 2_Programming_with_Python/Module_1/python_syntax.py
@@ -0,0 +1,16 @@
+print("Hello"); print("World")
+# Semi colon can be used to separate two lines of code
+
+
+x = 1 +       2 \
++ 3
+print(x)
+
+# Any amount of spaces or indentation is fine
+# However, when combining with additional lines, you need to give clear indicators of where the line has occurred
+
+
+# Indentation
+name = "John"
+if name == "John":
+    print(name) # Indentation is used within the code block
