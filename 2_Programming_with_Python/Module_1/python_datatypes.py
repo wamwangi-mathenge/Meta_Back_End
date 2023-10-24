043b8eff3316236d4bad1492b124b0a8ffb67b25
Brian <wamwangimathenge@gmail.com>
2023-10-24 09:43:42 +0300

    Experiment with different data types
    This commit looks into data types in python and how to determine the
    data type of a variable.

---
+++ 2_Programming_with_Python/Module_1/python_datatypes.py
@@ -0,0 +1,13 @@
+a = 10.0
+
+# To check the data type of a variable
+print(type(a)) # float
+
+b = 2.3
+print(type(b)) # string
+
+c = 'anbfqwhnkcdnls'
+print(type(c)) # string
+
+d = [1,2,3,4]
+print(type(d))
