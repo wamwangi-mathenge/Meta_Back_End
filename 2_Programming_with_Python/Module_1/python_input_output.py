7434dee01a66c9433753f61590298fe702b96c82
Brian <wamwangimathenge@gmail.com>
2023-10-25 15:37:17 +0300

    Learn about input and output functions
    This commit focuses on how to collect input from users using the
    input() function as well as how to use the print() function to display
    output

---
+++ 2_Programming_with_Python/Module_1/python_input_output.py
@@ -0,0 +1,12 @@
+num1 = input("Please enter the first number: ")
+num2 = input("Please enter a second number: ")
+
+print(num1, num2)
+print(int(num1) + int(num2))
+print(int(num1) - int(num2))
+
+
+string1 = input("Please enter your first name: ")
+string2 = input("Please enter your second name: ")
+
+print(f"Hello {string1} {string2}")
