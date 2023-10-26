55de449251df4d3b76b0e056d82c0b22d934a260
Brian <wamwangimathenge@gmail.com>
2023-10-26 10:56:59 +0300

    Iterate over a sequence using a for loop

---
+++ 2_Programming_with_Python/Module_1/python_for_loops.py
@@ -0,0 +1,10 @@
+# Python For Loops
+
+for i in range(10):
+    print(i)
+    
+    
+favorites = ['Creme Brulee', 'Apple Pie', 'Churros', 'Tiramiso', 'Chocolate Cake']
+
+for item in favorites:
+    print(item)
