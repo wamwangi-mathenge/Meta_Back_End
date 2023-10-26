e5222d97f228ef2a981c0962119e7243e73db2d8
Brian <wamwangimathenge@gmail.com>
2023-10-26 11:04:45 +0300

    Iterate over a sequence using a while loop

---
+++ 2_Programming_with_Python/Module_1/python_while_loop.py
@@ -0,0 +1,12 @@
+# Python While Loops
+
+favorites = ['Creme Brulee', 'Apple Pie', 'Churros', 'Tiramiso', 'Chocolate Cake']
+
+# As in the for loop, you need to specify a condition to make the loop over n times depending on the count itself
+
+# Declare a counter
+count = 0
+
+while count < len(favorites):
+    print(favorites[count])
+    count += 1
