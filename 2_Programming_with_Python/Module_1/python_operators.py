73233a7bd36625a9cfe9d3b28e9f349fc87acf43
Brian <wamwangimathenge@gmail.com>
2023-10-26 09:29:37 +0300

    Perform math and logical operators
    This commit covers the basic concepts in math and logical operators

---
+++ 2_Programming_with_Python/Module_1/python_operators.py
@@ -0,0 +1,49 @@
+# Math Operators in Python
+# Addition
+print(2 + 3)
+
+# Subtraction
+print(5 - 3)
+
+# Multiplication
+print(4 * 3)
+
+# Division
+print(9 / 3)
+
+
+# Logical Operators in Python
+# Example 1: And Operator
+a = True
+b = False
+
+if a and b:
+    print("All True")
+else:
+    print("Not all are True")
+    
+    
+# Example 2: Or Operator
+a = False
+b = False
+
+if a or b:
+    print("One is True")
+else:
+    print("None is True")
+    
+    
+# Example 3: Not Operator
+a = False
+b = True
+
+if not(a) or not(b):
+    print("At least one is False")
+    
+
+# Example 4: And Operator
+a = False
+b = False
+
+if not(a) and not(b):
+    print("All are False")
