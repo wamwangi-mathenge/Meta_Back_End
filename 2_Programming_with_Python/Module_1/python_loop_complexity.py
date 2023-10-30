1a73110cb9c83221618144406bd23f2fc3c31caa
Brian <wamwangimathenge@gmail.com>
2023-10-30 08:41:32 +0300

    Determine the time taken for the loop to run

--- 2_Programming_with_Python/Module_1/python_loop_complexity.py
+++ 2_Programming_with_Python/Module_1/python_loop_complexity.py
@@ -1,6 +1,12 @@
+import time
+start_time = time.time()
+
 # Outer loop
-for i in range(10):
+for i in range(100):
     # Inner loop
-    for j in range(10):
+    for j in range(1000):
         print(0, end = " ")
     print()
+    
+    
+print(round(time.time() - start_time), 2)
