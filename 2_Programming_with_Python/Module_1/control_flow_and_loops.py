f243734a4438eb3e62317ebb10aae7896a6dd264
Brian <wamwangimathenge@gmail.com>
2023-10-30 08:09:11 +0300

    Use continue to control the loop's iteration
    This commit uses continue to control the iteration of the loop. It
    allows one to skip over a section of the loop but then continue with
    the rest

--- 2_Programming_with_Python/Module_1/control_flow_and_loops.py
+++ 2_Programming_with_Python/Module_1/control_flow_and_loops.py
@@ -20,6 +20,7 @@ favorites = ["Nairobi", "Cape Town", "Cairo", "Casablanca", "Windhoek"]
 #         print("Kenya's capital does not exist")
         
 
+# -----BREAK-------
 # Flaw in the above logic
 # Change the search term to something that is on the list eg. Nairobi
 # The city is on the list, but it still printed out the else condition
@@ -36,3 +37,16 @@ for city in favorites:
 # Running the above code will resolve the issue.
 # The BREAK statement is used to stop the loop, which in turn stops the condition.
 # Without the break, the loop will continue even after the `if` condition is met
+
+
+# ------CONTINUE-------
+# Similar to break, continue can be used to control the iteration of the loop.
+# The key difference is that it can allow you to skip over a section of the loop but then continue on with the rest
+
+
+for city in favorites:
+    if city == "Cairo":
+        continue
+    print(f"Other cities in Africa are {city}")
+    
+# The outcome of the above code snippet will print everything except the Churros dessert
