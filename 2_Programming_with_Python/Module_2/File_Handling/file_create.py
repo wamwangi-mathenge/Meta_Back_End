53efa8e0b66f232765aebd797dc3fce50e11b1fe
Brian <wamwangimathenge@gmail.com>
2023-11-18 14:12:30 +0300

    Trap any errors in file creation program

--- 2_Programming_with_Python/Module_2/File_Handling/file_create.py
+++ 2_Programming_with_Python/Module_2/File_Handling/file_create.py
@@ -1,2 +1,9 @@
-with open("newfile.txt", "w") as file:
-    file.writelines(["Hello Brian!!!", "\nThis is a new file!!!"])
+# with open("newfile.txt", "w") as file:
+#     file.writelines(["Hello Brian!!!", "\nThis is a new file!!!"])
+    
+    
+try:
+    with open("/sample/newfile.txt", "w") as file:
+        file.writelines(["Hello Brian!!!", "\nThis is a new file!!!"])
+except Exception as e:
+    print(e, "File not found")
