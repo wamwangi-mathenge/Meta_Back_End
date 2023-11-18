c47d2e4d36cb8012eae978211b8161d712d1588b
Brian <wamwangimathenge@gmail.com>
2023-11-18 13:34:06 +0300

    Trap for different exception in ZeroDivisionError

--- 2_Programming_with_Python/Module_2/Exceptions_in_Python/main.py
+++ 2_Programming_with_Python/Module_2/Exceptions_in_Python/main.py
@@ -11,13 +11,15 @@ except:
     
 # ZeroDivisionError
 def divide_by(a, b):
-    return a / b
-
-try:
-    ans = divide_by(40, 0)
-    print(ans)
-except:
-    print(0)
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return 0
+    except Exception as e:
+        print("Something went wrong")
+        
+ans = divide_by(10, 0)
+print(ans)
     
     
 # FileNotFoundError
