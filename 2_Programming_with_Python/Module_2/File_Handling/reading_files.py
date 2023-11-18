10c33f3a3e0a7c27f5334b1bad1a1266d4c6d3f3
Brian <wamwangimathenge@gmail.com>
2023-11-18 14:26:57 +0300

    Demonstrate how to output different formats

---
+++ 2_Programming_with_Python/Module_2/File_Handling/reading_files.py
@@ -0,0 +1,13 @@
+# read() => Returns the entire contents of the file as a string that will contain all the characters.
+# with open('newfile.txt', 'r') as file:
+    # print(file.read())
+    # You can also pass in an integer to return only the specified characters in the file
+    # print(file.read(40))
+    
+# readline() => Returns a single line as a string.
+    # print(file.readline())
+    # You can also pass in an integer to return a specified number of characters in a single line
+    # print(file.readline(6))
+    
+# readlines() => Returns the entire contents of the file and then returns it in an ordered list
+    # print(file.readlines())
